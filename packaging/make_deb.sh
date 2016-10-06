af34dbdf0ecaa53fd53e6b5145246279cd3fa534
Marcello Mamino <vacaboja@gmail.com>
2016-10-06 02:40:34 +0200

    Fixed Debian scripts

--- packaging/make_deb.sh
+++ packaging/make_deb.sh
@@ -3,13 +3,18 @@
 DIR=`dirname "${BASH_SOURCE[0]}"`
 ABSDIR=`cd "$DIR"; pwd`
 
-cd $DIR/..
+cd "$DIR"/..
 
 VERSION=`cat version`
 
 cd build
-cp tg-timer_$VERSION.tar.gz tg-timer_$VERSION.orig.tar.gz
-tar xzf tg-timer_$VERSION.orig.tar.gz
-cp -r $ABSDIR/debian tg-timer-$VERSION
-cd tg-timer-$VERSION
+rm -rf deb
+mkdir deb
+cp tg-timer_"$VERSION".tar.gz deb/tg-timer_"$VERSION".orig.tar.gz
+
+cd deb
+
+tar xzf tg-timer_"$VERSION".orig.tar.gz
+cp -r "$ABSDIR"/debian tg-timer-"$VERSION"
+cd tg-timer-"$VERSION"
 debuild -us -uc
--- packaging/make_tgz.sh
+++ packaging/make_tgz.sh
@@ -3,8 +3,8 @@
 DIR=`dirname "${BASH_SOURCE[0]}"`
 ABSDIR=`cd "$DIR"; pwd`
 
-cd $DIR/..
+cd "$DIR"/..
 
 VERSION=`cat version`
 
-tar czf build/tg-timer_$VERSION.tar.gz * --exclude=".*" --exclude="build/*" --exclude="packaging" --xform="s|\\(.*\\)|tg-timer-$VERSION/\1|"
+tar czf build/tg-timer_"$VERSION".tar.gz * --exclude=".*" --exclude="build/*" --exclude="packaging" --xform="s|\\(.*\\)|tg-timer-$VERSION/\1|"
