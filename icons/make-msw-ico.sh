73d9e4096f293c83ff95bf8f72352963e67668b8
Marcello Mamino <vacaboja@gmail.com>
2017-06-08 21:23:31 +0200

    Trying to register tgj extension under windows

--- icons/make-msw-ico.sh
+++ icons/make-msw-ico.sh
@@ -5,3 +5,7 @@ SIZES=`cat sizes`
 PNGS=`for A in $SIZES; do echo $[A]x$[A]/tg-timer.png; done`
 
 icotool -c -o tg-timer.ico $PNGS
+
+PNGS=`for A in $SIZES; do echo $[A]x$[A]/tg-document.png; done`
+
+icotool -c -o tg-document.ico $PNGS
--- packaging/make_msi.sh
+++ packaging/make_msi.sh
@@ -12,6 +12,7 @@ ABSDIR=`cd "$DIR"; pwd`
 RESOURCES=`cd "$1"; pwd`
 
 TARGET="$ABSDIR/../build/msi"
+TMP="$ABSDIR/../build/tmp"
 
 cd "$ABSDIR"/..
 
@@ -20,14 +21,17 @@ VERSION=`cat version`
 make
 
 rm -rf "$TARGET"
+rm -rf "$TMP"
 mkdir -p "$TARGET"
 cp "$ABSDIR/tg-timer.wxs" "$TARGET"
 cp "$ABSDIR/LICENSE.rtf" "$TARGET"
 cp "$ABSDIR/../README.md" "$TARGET"
 cp "$ABSDIR/../LICENSE" "$TARGET"
 cp "$ABSDIR/../build/tg.exe" "$TARGET"
-cp -r "$RESOURCES"/* "$TARGET"
-heat dir "$RESOURCES" -srd -gg -sreg -dr INSTALLDIR -cg Resources -out "$TARGET/Resources.wxs"
+cp -r "$RESOURCES" "$TMP"
+cp "$ABSDIR/../icons/tg-document.ico" "$TMP"
+heat dir "$TMP" -srd -gg -sreg -dr INSTALLDIR -cg Resources -out "$TARGET/Resources.wxs"
+mv "$TMP"/* "$TARGET"
 
 cd "$TARGET"
 
